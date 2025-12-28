a1024e5600469073a96cc576dd4be1688b1c6f57
KrishnaPatel <kp06krish@gmail.com>
2025-12-28 19:53:40 +0530

    added skills section

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -1,11 +1,173 @@
-import React from 'react'
+import GlareHover from "@/components/GlareHover";
+import React from "react";
 
 const Skills = () => {
   return (
-    <div>
-      
+    <div className="px-22 text-white bg-black">
+      <div className="py-6 ">
+        <div className="text-[#2a2a2a] text-xs">// key_skills={"["}</div>
+        <div className="py-2">
+          <div className="flex items-center gap-2 flex-wrap">
+            <GlareHover
+              glareColor="#ffffff"
+              glareOpacity={0.3}
+              glareAngle={-30}
+              glareSize={200}
+              transitionDuration={800}
+              playOnce={false}
+            >
+              React
+            </GlareHover>
+            <GlareHover
+              glareColor="#ffffff"
+              glareOpacity={0.3}
+              glareAngle={-30}
+              glareSize={200}
+              transitionDuration={800}
+              playOnce={false}
+            >
+              Javascript
+            </GlareHover>
+            <GlareHover
+              glareColor="#ffffff"
+              glareOpacity={0.3}
+              glareAngle={-30}
+              glareSize={200}
+              transitionDuration={800}
+              playOnce={false}
+            >
+              Tailwind CSS
+            </GlareHover>
+            <GlareHover
+              glareColor="#ffffff"
+              glareOpacity={0.3}
+              glareAngle={-30}
+              glareSize={200}
+              transitionDuration={800}
+              playOnce={false}
+            >
+              Node.js
+            </GlareHover>
+            <GlareHover
+              glareColor="#ffffff"
+              glareOpacity={0.3}
+              glareAngle={-30}
+              glareSize={200}
+              transitionDuration={800}
+              playOnce={false}
+            >
+              Express
+            </GlareHover>
+            <GlareHover
+              glareColor="#ffffff"
+              glareOpacity={0.3}
+              glareAngle={-30}
+              glareSize={200}
+              transitionDuration={800}
+              playOnce={false}
+            >
+              MongoDB
+            </GlareHover>
+            <GlareHover
+              glareColor="#ffffff"
+              glareOpacity={0.3}
+              glareAngle={-30}
+              glareSize={200}
+              transitionDuration={800}
+              playOnce={false}
+            >
+              Git
+            </GlareHover>
+            <GlareHover
+              glareColor="#ffffff"
+              glareOpacity={0.3}
+              glareAngle={-30}
+              glareSize={200}
+              transitionDuration={800}
+              playOnce={false}
+            >
+              Github
+            </GlareHover>
+            <GlareHover
+              glareColor="#ffffff"
+              glareOpacity={0.3}
+              glareAngle={-30}
+              glareSize={200}
+              transitionDuration={800}
+              playOnce={false}
+            >
+              Java
+            </GlareHover>
+            {/* <GlareHover
+              glareColor="#ffffff"
+              glareOpacity={0.3}
+              glareAngle={-30}
+              glareSize={200}
+              transitionDuration={800}
+              playOnce={false}
+            >
+              Github
+            </GlareHover>
+            <GlareHover
+              glareColor="#ffffff"
+              glareOpacity={0.3}
+              glareAngle={-30}
+              glareSize={200}
+              transitionDuration={800}
+              playOnce={false}
+            >
+              Github
+            </GlareHover>
+            <GlareHover
+              glareColor="#ffffff"
+              glareOpacity={0.3}
+              glareAngle={-30}
+              glareSize={200}
+              transitionDuration={800}
+              playOnce={false}
+            >
+              Github
+            </GlareHover>
+            <GlareHover
+              glareColor="#ffffff"
+              glareOpacity={0.3}
+              glareAngle={-30}
+              glareSize={200}
+              transitionDuration={800}
+              playOnce={false}
+            >
+              Github
+            </GlareHover> */}
+          </div>
+        </div>
+        <div className="text-[#2a2a2a] text-xs ">// {"]"}</div>
+      </div>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
     </div>
-  )
-}
+  );
+};
 
-export default Skills
+export default Skills;
