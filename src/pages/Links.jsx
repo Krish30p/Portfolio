6e3741666770316c41495266a131cfcd4855c20f
KrishnaPatel <kp06krish@gmail.com>
2025-12-30 23:17:17 +0530

    added links

--- src/pages/Links.jsx
+++ src/pages/Links.jsx
@@ -3,29 +3,70 @@ import React from "react";
 const Links = () => {
   return (
     <div className="bg-black text-white px-22 py-6 border-t border-[#2a2a2a]">
-      <div>
+      <div className="space-y-4">
         
           {/* what? */}
-          <div className="text-xl">
+          <div className="text-xl mb">
             <span className="text-gray-600">//</span>
             <span>links</span>
           </div>
           {/* Links */}
           <div>
-            <div>
-                <div className="flex flex-col md:flex-row md:items-center md:justify-between gap-2">
+            <div className="space-y-4 ">
+                <div className="py-4 flex flex-col md:flex-row md:items-center md:justify-between gap-2 text-3xl">
                     <span className="space-x-3">
                         {/* <span className="">agsvd</span> */}
-                        <span>Github</span>
+                        <span className="text-[#4a4a4a]">const{''}</span>
+                        <span >linkedIn{''}</span>
+                        <span className="text-[#4a4a4a]">=</span>
+                    </span>
+                    <a
+                    href="https://www.linkedin.com/in/krishna-patel-0ba60132a/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="flex items-center gap-1 text-white cursor-pointer hover:text-gray-400"
+                  >
+                    <span className="text-[#2a2a2a]">'</span>
+                    <span className="text-[#c0c0c0]">LinkedIn.com/in/krishna-patel</span>
+                    <span className="text-[#2a2a2a]">'</span>
+                  </a>
+                    
+                </div>
+                <div className="flex flex-col md:flex-row md:items-center md:justify-between gap-2 text-3xl">
+                    <span className="space-x-3">
+                        {/* <span className="">agsvd</span> */}
+                        <span className="text-[#4a4a4a]">const{''}</span>
+                        <span>github{''}</span>
+                        <span className="text-[#4a4a4a]">=</span>
                     </span>
                     <a
                     href="https://github.com/Krish30p"
                     target="_blank"
                     rel="noopener noreferrer"
-                    className="flex items-center gap-2 text-white cursor-pointer hover:text-gray-400"
+                    className="flex items-center gap-1 text-white cursor-pointer hover:text-gray-400"
                   >
+                    <span className="text-[#2a2a2a]">'</span>
+                    <span className="text-[#c0c0c0]">GitHub.com/Krish30p</span>
+                    <span className="text-[#2a2a2a]">'</span>
+                  </a>
                     
-                    <span>GitHub.com/Krish30p</span>
+                </div>
+                <div className="py-4 flex flex-col md:flex-row md:items-center md:justify-between gap-2 text-3xl">
+                    <span className="space-x-3">
+                        {/* <span className="">agsvd</span> */}
+                        <span className="text-[#4a4a4a]">const{''}</span>
+                        <span>email{''}</span>
+                        <span className="text-[#4a4a4a]">=</span>
+                    </span>
+                    <a
+                    href="mailto:kp06krish@gmail.com"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="flex items-center gap-1 text-white cursor-pointer hover:text-gray-400"
+                  >
+                    <span className="text-[#2a2a2a]">'</span>
+                    <span className="text-[#c0c0c0]">kp06krish@gmail.com</span>
+                    <span className="text-[#2a2a2a]">'</span>
                   </a>
                     
                 </div>
